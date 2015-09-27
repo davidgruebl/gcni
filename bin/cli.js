5dbd0d28e56ffdb3d990bda66b7a491f4064df51
David Thomas Grübl <me@davidgruebl.com>
2015-09-27 00:54:03 -0700

    style: standard

--- bin/cli.js
+++ bin/cli.js
@@ -20,7 +20,7 @@ var cmd = [
   'npm install'
 ]
 
-exec(cmd.join(' && '), {}, function(err){
-  if(err) return console.error(err)
+exec(cmd.join(' && '), {}, function (err) {
+  if (err) return console.error(err)
   console.log('\u270C done \u270C')
 })
